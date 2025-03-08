41a172a0179cc55d175229b11d08c650f07ba5e5
Váltson Araújo <valtson88@gmail.com>
2025-03-08 19:54:16 -0300

    Update register.js

--- paginas/src/register.js
+++ paginas/src/register.js
@@ -1,3 +1,4 @@
+const API_URL = process.env.API_URL
 const form = document.getElementById("register")
 
 form.addEventListener("submit", async (ev) => {
@@ -8,7 +9,7 @@ form.addEventListener("submit", async (ev) => {
     params.append("email", document.getElementById("email").value);
     params.append("password", document.getElementById("password").value)
 
-    fetch("http://127.0.0.1:8000/register/", {
+    fetch(`${API_URL}/register/`, {
     method: "POST",
     headers: {
         "Content-Type": "application/x-www-form-urlencoded",
